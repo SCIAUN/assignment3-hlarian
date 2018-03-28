103bae79c3da94f1ee4579484249680b436abc94
hlarian <36689969+hlarian@users.noreply.github.com>
2018-03-28 18:28:35 +0430

    Update assignment3.py

--- assignment3.py
+++ assignment3.py
@@ -2,13 +2,26 @@ import socket
 
 ports=[21,22,25,80,110,9090]
 socket.setdefaulttimeout(5)
-f=open('host_names','r')
-host_names=f.read()
+host_file=open('host_names','r')
+host_names=host_file.read()
 host_names=host_names.split('\n')
 
-for x in host_names:
-    for y in ports:
+s = socket.socket()
+
+for host in host_names:
+    for port in ports:
         try:
-           # you should write your codes in this block
+            Respons_name = "%s-%s.txt" % (host, port)
+            Response_file = open(Respons_name, 'wb')
+
+            s = socket.socket()
+            s.connect((host, port))
+
+            rec = s.recv(1024)
+            Response_file.write(rec)
+
+            Response_file.close()
+            s.close
+
         except Exception as e:
             print(e)
